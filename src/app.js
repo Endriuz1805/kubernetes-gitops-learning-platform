4e34ea9692d312cd17734cf5f5db274d508f2547
Andrei Apostol <a.apostol.extern@rewe-group.at>
2025-07-23 15:29:08 +0300

    Fix ESLint error: Mark unused 'next' parameter with   underscore

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ app.put('/topics/:id/complete', (req, res) => {
 });
 
 // Error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, req, res, _next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
